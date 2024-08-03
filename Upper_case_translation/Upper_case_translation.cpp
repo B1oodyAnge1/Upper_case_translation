e6c3cf0ccf265fc624f37b66fa31313fcd0944ff
B1oodyAnge1 <romsm@bk.ru>
2024-08-03 16:53:16 +0300

    The task is completed

--- Upper_case_translation/Upper_case_translation.cpp
+++ Upper_case_translation/Upper_case_translation.cpp
@@ -1,8 +1,20 @@
 ﻿#include <iostream>
+#include <string>
+#include <algorithm>
+
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "ru");
+    std::string str = "";
+    std::cout << "Введите букву нижнего регистра: \n";
+    std::cin >> str;
+    std::transform(str.begin(), str.end(), str.begin(), toupper);
+    std::cout << str << std::endl;
+    return 0;
+    
 }
 
 
